f48e24578d082c8f13098d7618b97d3862ae8ae5
alfisahr <alfin.andri@gmail.com>
2024-02-07 21:39:25 +0700

    Setup tailwindcss with daisyui plugin

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "Roboto", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+};
