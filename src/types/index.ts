df7d27282c628e7c42c7636320bc201423e0847a
alfisahr <alfin.andri@gmail.com>
2024-02-08 19:46:07 +0700

    Creating movie types and its corresponding types

---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+import type { Movie } from "./Movie";
+import type { MoviesResponse } from "./MoviesResponse";
+import type { Genre } from "./Genre";
+import type { Credit } from "./Credit";
+import type { Review } from "./Review";
+
+export { MoviesResponse, Movie, Genre, Credit, Review };
