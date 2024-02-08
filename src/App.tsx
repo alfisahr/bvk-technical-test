c35dea2842fad2a0b0a87d73d6d6aab66b645c21
alfisahr <alfin.andri@gmail.com>
2024-02-08 07:18:51 +0700

    Setup API srvice, pointing to themovie.org, including configuring secret & key into .env

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
+import apiClient from "./services/apiClient";
 
 function App() {
+  useEffect(() => {
+    async function fetchData() {
+      const res = await apiClient.get("/trending/movie/day");
+      console.log(res);
+    }
+    fetchData();
+    // console.log("dijalankan");
+  }, []);
+
   return (
     <>
       <div className="text-red-500">tes</div>
