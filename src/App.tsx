fa2643218fa793f62d3994a3bd517e92d492105b
alfisahr <alfin.andri@gmail.com>
2024-02-09 07:19:47 +0700

    Update App.tsx: implementing context to the whole app through App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./utils/routes";
+import { useReducer } from "react";
+import watchedReducer from "./states/watchedReducer";
+import AppContext from "./states/appContext";
+import moviesReducer from "./states/moviesReducer";
+import appReducer, { GeneralAppState } from "./states/appReducer";
 
 function App() {
-  return <RouterProvider router={router} />;
+  const [watchedMovies, dispatchWatched] = useReducer(watchedReducer, []);
+  const [movies, dispatchMovies] = useReducer(moviesReducer, []);
+  const [appState, dispatchApp] = useReducer(appReducer, {} as GeneralAppState);
+  return (
+    <AppContext.Provider
+      value={{
+        watchedMovies,
+        dispatchWatched,
+        movies,
+        dispatchMovies,
+        appState,
+        dispatchApp,
+      }}
+    >
+      <RouterProvider router={router} />
+    </AppContext.Provider>
+  );
 }
 
 export default App;
